86e351647464eedf763faba88551c246331bb956
Kennedy Shearer <kshea009@gmail.com>
2026-01-20 16:44:14 -0500

    bucket list tracker app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,156 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react"
+import {
+  Authenticator,
+  Button,
+  Text,
+  TextField,
+  Heading,
+  Flex,
+  View,
+  Image,
+  Grid,
+  Divider,
+} from "@aws-amplify/ui-react"
+import "@aws-amplify/ui-react/styles.css"
 
-function App() {
-  const [count, setCount] = useState(0)
+import { getUrl, uploadData } from "aws-amplify/storage"
+import { generateClient } from "aws-amplify/data"
+
+/**
+ * @type {import('aws-amplify/data').Client<import('../amplify/data/resource').Schema>}
+ */
+
+export default function App() {
+  // ✅ MUST be inside component
+  const client = generateClient({
+    authMode: "userPool",
+  })
+
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    fetchItems()
+  }, [])
+
+  async function fetchItems() {
+    const { data } = await client.models.BucketItem.list()
+
+    await Promise.all(
+      data.map(async (item) => {
+        if (item.image) {
+          const link = await getUrl({
+            path: ({ identityId }) => `media/${identityId}/${item.image}`,
+          })
+          item.image = link.url
+        }
+        return item
+      })
+    )
+
+    setItems(data)
+  }
+
+  async function createItem(event) {
+    event.preventDefault()
+    const form = new FormData(event.target)
+
+    const { data: newItem } = await client.models.BucketItem.create({
+      title: form.get("title"),
+      description: form.get("description"),
+      image: form.get("image").name,
+    })
+
+    if (newItem.image) {
+      await uploadData({
+        path: ({ identityId }) => `media/${identityId}/${newItem.image}`,
+        data: form.get("image"),
+      }).result
+    }
+
+    fetchItems()
+    event.target.reset()
+  }
+
+  async function deleteItem(item) {
+    await client.models.BucketItem.delete({ id: item.id })
+    fetchItems()
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Authenticator>
+      {({ signOut }) => (
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          direction="column"
+          width="70%"
+          margin="0 auto"
+        >
+          <Heading level={1}>My Bucket List</Heading>
+
+          <View as="form" margin="3rem 0" onSubmit={createItem}>
+            <Flex direction="column" gap="2rem" padding="2rem">
+              <TextField
+                name="title"
+                placeholder="Bucket List Item"
+                labelHidden
+                required
+              />
+              <TextField
+                name="description"
+                placeholder="Description"
+                labelHidden
+                required
+              />
+              <View
+                as="input"
+                name="image"
+                type="file"
+                accept="image/png, image/jpeg"
+              />
+              <Button type="submit">Add to Bucket List</Button>
+            </Flex>
+          </View>
+
+          <Divider />
+
+          <Heading level={2}>My Bucket List Items</Heading>
+
+          <Grid margin="3rem 0" gap="2rem">
+            {items.map((item) => (
+              <Flex
+                key={item.id}
+                direction="column"
+                alignItems="center"
+                gap="1rem"
+                border="1px solid #ccc"
+                padding="2rem"
+                borderRadius="10px"
+              >
+                <Heading level={3}>{item.title}</Heading>
+                <Text fontStyle="italic">{item.description}</Text>
+
+                {item.image && (
+                  <Image
+                    src={item.image}
+                    alt={item.title}
+                    style={{ width: 400 }}
+                  />
+                )}
+
+                <Button
+                  variation="destructive"
+                  onClick={() => deleteItem(item)}
+                >
+                  Delete Item
+                </Button>
+              </Flex>
+            ))}
+          </Grid>
+
+          <Button onClick={signOut}>Sign Out</Button>
+        </Flex>
+      )}
+    </Authenticator>
   )
 }
-
-export default App
